241e68832460ef18bec93a9e0a10a5bf0857ea29
non <chenry@mcs.anl.gov>
2014-08-10 15:53:15 -0500

    Improving caching in script

--- BiomassAnalysis/GapfillWSModels.pl
+++ BiomassAnalysis/GapfillWSModels.pl
@@ -8,11 +8,11 @@ use Bio::KBase::fbaModelServices::ScriptHelpers qw(get_ws_objects_list fbaws get
 my $workspace = $ARGV[0];
 my $procindex = $ARGV[1];
 my $numprocs = $ARGV[2];
+my $fba = get_fba_client();
 my $models = get_ws_objects_list($workspace,"KBaseFBA.FBAModel");
 for (my $i=0; $i < @{$models}; $i++) {
 	my $value = $i-$procindex;
 	if (($value % $numprocs) == 0) {
-		my $fba = get_fba_client();
 		my $output = $fba->adjust_biomass_reaction({
 			compounds => ["cpd11715","cpd11746","cpd09680"],
 			model => $models->[$i]->[1],

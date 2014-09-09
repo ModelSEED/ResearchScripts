166f3a9a843192306ae77815a198569d6dda6412
non <chenry@mcs.anl.gov>
2014-09-08 23:54:26 -0500

    Script to build models for all CS genomes

---
+++ BuildCSModels/BuildCSModels.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Bio::KBase::workspace::ScriptHelpers qw(printObjectInfo get_ws_client workspace workspaceURL parseObjectMeta parseWorkspaceMeta printObjectMeta);
+use Bio::KBase::fbaModelServices::ScriptHelpers qw(get_ws_objects_list fbaws get_fba_client runFBACommand universalFBAScriptCode );
+
+my $procindex = $ARGV[0];
+my $numprocs = $ARGV[1];
+my $fba = get_fba_client();
+
+my $genomes = get_ws_objects_list("KBasePublicGenomesV4","KBaseGenomes.Genome");
+
+for (my $i=0; $i < @{$genomes}; $i++) {
+	my $value = $i-$procindex;
+	if (($value % $numprocs) == 0) {
+		my $output = $fba->genome_to_fbamodel({
+			genome => $genomes->[$i],
+			workspace => "KBasePublicModelsV4",
+			model => $genomes->[$i].".model"
+	   	});
+		printObjectInfo($output);
+		$output = $fba->gapfill_model({
+			model => $genomes->[$i].".model",
+			workspace => "KBasePublicModelsV4",
+			out_model => $genomes->[$i].".model.gf",
+			solver => "cplex",
+			fastgapfill => 1,
+			integrate_solution => 1
+		});
+		printObjectInfo($output);
+	}
+}
+
+
+		

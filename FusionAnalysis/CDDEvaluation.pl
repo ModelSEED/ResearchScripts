15d892ba14f8d5127d50dfe65daee0aede39fc83
non <chenry@mcs.anl.gov>
2015-03-13 00:51:41 -0500

    Fixing CDD script

--- FusionAnalysis/CDDEvaluation.pl
+++ FusionAnalysis/CDDEvaluation.pl
@@ -5,9 +5,9 @@ my $directory = $ARGV[0];
 my $inputdir = $ARGV[1];
 
 my $CDDData = {};
-my $functions = {};
 my $cddfunctions = {};
 my $array;
+my $cddoverlap;
 open(my $fh, "<", $inputdir."GenomeList.txt");
 while (my $line = <$fh>) {
 	chomp($line);
@@ -20,6 +20,7 @@ for (my $i=0; $i < @{$array}; $i++) {
 	open($fh, "<", $inputdir.$array->[$i]);
 	my $line = <$fh>;
 	my $correlate = {};
+	my $genes = {};
 	while (my $line = <$fh>) {
 		chomp($line);
 		my $items = [split(/\t/,$line)];
@@ -37,6 +38,7 @@ for (my $i=0; $i < @{$array}; $i++) {
 			$functions->{$items->[6]}->{$items->[8]} = 0;
 			$cddfunctions->{$items->[8]}->{$items->[6]} = 0;
 		}
+		$genes->{$items->[0]}->{$items->[8]} = [$items->[3],$items->[4]];
 		$functions->{$items->[6]}->{$items->[8]} += $maxalign*$items->[5];
 		$cddfunctions->{$items->[8]}->{$items->[6]} += $maxalign*$items->[5];
 		$CDDData->{$items->[8]}->{genecount}++;
@@ -48,8 +50,54 @@ for (my $i=0; $i < @{$array}; $i++) {
 			$CDDData->{$items->[8]}->{maxalign} = $maxalign;
 		}
 	}
+	foreach my $gene (keys(%{$genes})) {
+		my $genedata = $genes->{$gene};
+		my $cddkeys = [keys(%{$genedata})];
+		for (my $j=0; $j < @{$cddkeys}; $j++) {
+			for (my $k=$j+1; $k < @{$cddkeys}; $k++) {
+				my $overstart = $genedata->{$cddkeys->[$j]}->[0];
+				if ($genedata->{$cddkeys->[$k]}->[0] > $overstart) {
+					$overstart = $genedata->{$cddkeys->[$k]}->[0];
+				}
+				my $overend = $genedata->{$cddkeys->[$j]}->[1];
+				if ($genedata->{$cddkeys->[$k]}->[1] < $overend) {
+					$overend = $genedata->{$cddkeys->[$k]}->[1];
+				}
+				my $overlap = $overend - $overstart;
+				if ($overlap > 0) {
+					my $overfrac = $overlap/($genedata->{$cddkeys->[$j]}->[1]-$genedata->{$cddkeys->[$j]}->[0]);
+					if ($overfrac >= 0.7) {
+						if (!defined($cddoverlap->{$cddkeys->[$j]}->{$cddkeys->[$k]})) {
+							$cddoverlap->{$cddkeys->[$j]}->{$cddkeys->[$k]} = [0,0,0];
+						}
+						$cddoverlap->{$cddkeys->[$j]}->{$cddkeys->[$k]}->[0]++;
+						if ($overfrac >= 0.8) {
+							$cddoverlap->{$cddkeys->[$j]}->{$cddkeys->[$k]}->[1]++;
+							if ($overfrac >= 0.9) {
+								$cddoverlap->{$cddkeys->[$j]}->{$cddkeys->[$k]}->[2]++;
+							}
+						}
+					}
+					$overfrac = $overlap/($genedata->{$cddkeys->[$k]}->[1]-$genedata->{$cddkeys->[$k]}->[0]);
+					if ($overfrac >= 0.7) {
+						if (!defined($cddoverlap->{$cddkeys->[$k]}->{$cddkeys->[$j]})) {
+							$cddoverlap->{$cddkeys->[$k]}->{$cddkeys->[$j]} = [0,0,0];
+						}
+						$cddoverlap->{$cddkeys->[$k]}->{$cddkeys->[$j]}->[0]++;
+						if ($overfrac >= 0.8) {
+							$cddoverlap->{$cddkeys->[$k]}->{$cddkeys->[$j]}->[1]++;
+							if ($overfrac >= 0.9) {
+								$cddoverlap->{$cddkeys->[$k]}->{$cddkeys->[$j]}->[2]++;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 }
 
+my $functions = {};
 open(GENELIST, ">", $directory."GeneList.txt");
 open(CDDFUNCS, ">", $directory."CDDFunctions.txt");
 open(CDDLIST, ">", $directory."CDDList.txt");
@@ -59,16 +107,28 @@ print CDDLIST "CDD\tMax align\tSingleGenes\tTotalGenes\n";
 foreach my $key (keys(%{$CDDData})) {
 	print CDDFUNCS $key."\t";
 	my $first = 0;
+	my $array = [];
+	my $sum = 0;
 	foreach my $func (keys(%{$cddfunctions->{$key}})) {
-		print CDDFUNCS $func.":::".$cddfunctions->{$key}->{$func};
+		$sum += $cddfunctions->{$key}->{$func};
+		push(@{$array},[$func,$cddfunctions->{$key}->{$func}]);
+	}
+	for (my $i=0; $i < @{$array}; $i++) {
+		$array->[$i]->[1] = $array->[$i]->[1]/$sum;
+		$cddfunctions->{$key}->{$array->[$i]->[0]} = $array->[$i]->[1];
+		$functions->{$array->[$i]->[0]}->{$key} = $array->[$i]->[1];
+	}
+	my $sortedarray = [sort { $b->[1] <=> $a->[1] } @{$array}];
+	for (my $i=0; $i < @{$sortedarray}; $i++) {
 		if ($first != 0) {
 			print CDDFUNCS ";;;";
 		}
+		print CDDFUNCS $sortedarray->[$i]->[0].":::".$sortedarray->[$i]->[1];
 		$first = 1;
 	}
 	print CDDFUNCS "\n";
-	print GENELIST $key."\t".join(";",@{$CDDData->{$items->[8]}->{singlegenes}})."\n";
-	print CDDLIST $key."\t".$CDDData->{$items->[8]}->{maxalign}."\t".$CDDData->{$items->[8]}->{singlegenecount}."\t".$CDDData->{$items->[8]}->{genecount}."\n";
+	print GENELIST $key."\t".join(";",@{$CDDData->{$key}->{singlegenes}})."\n";
+	print CDDLIST $key."\t".$CDDData->{$key}->{maxalign}."\t".$CDDData->{$key}->{singlegenecount}."\t".$CDDData->{$key}->{genecount}."\n";
 }
 close(CDDLIST);
 close(GENELIST);
@@ -79,15 +139,34 @@ print FUNCCDDS "Function\tCDDs\n";
 foreach my $key (keys(%{$functions})) {
 	print FUNCCDDS $key."\t";
 	my $first = 0;
+	my $array = [];
+	my $sum = 0;
 	foreach my $cdd (keys(%{$functions->{$key}})) {
-		print FUNCCDDS $cdd.":::".$functions->{$key}->{$cdd};
+		$sum += $functions->{$key}->{$cdd};
+		push(@{$array},[$cdd,$functions->{$key}->{$cdd}]);
+	}
+	for (my $i=0; $i < @{$array}; $i++) {
+		$array->[$i]->[1] = $array->[$i]->[1]/$sum;
+		$functions->{$key}->{$array->[$i]->[0]} = $array->[$i]->[1];
+	}
+	my $sortedarray = [sort { $b->[1] <=> $a->[1] } @{$array}];
+	for (my $i=0; $i < @{$sortedarray}; $i++) {
 		if ($first != 0) {
 			print FUNCCDDS ";;;";
 		}
+		print FUNCCDDS $sortedarray->[$i]->[0].":::".$sortedarray->[$i]->[1];
 		$first = 1;
 	}
 	print FUNCCDDS "\n";
 }
 close(FUNCCDDS);
+
+open(CDDSETS, ">", $directory."CDDSets.txt");
+foreach my $key (keys(%{$cddoverlap})) {
+	foreach my $cdd (keys(%{$cddoverlap->{$key}})) {
+		print $key."\t".$cdd."\t".join("\t",@{$cddoverlap->{$key}->{$cdd}})."\t".$CDDData->{$key}->{genecount}."\n";
+	}
+}
+close(CDDSETS);
 
 1;

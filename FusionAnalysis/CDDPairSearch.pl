dce5de065843109e24983aa8d2e0ace4c96ed82b
non <chenry@mcs.anl.gov>
2015-03-22 22:01:11 -0500

    Adjusting cdd pair search script

--- FusionAnalysis/CDDPairSearch.pl
+++ FusionAnalysis/CDDPairSearch.pl
@@ -3,6 +3,7 @@
 $|=1;
 my $directory = $ARGV[0];
 my $inputdir = $ARGV[1];
+my $genome = $ARGV[2];
 
 my $CDDData = {};
 my $cddfunctions = {};
@@ -13,12 +14,19 @@ while (my $line = <$fh>) {
 	push(@{$array},$line);
 }
 close($fh);
+if (defined($genome)) {
+	$array = [split(";",$genome)];
+}
 my $pairhash = {};
+my $triples = {};
 open(my $fhh, "<", $inputdir."CDDPairs.txt");
 while (my $line = <$fhh>) {
 	chomp($line);
 	my $temparray = [split(/\t/,$line)];
 	$pairhash->{$temparray->[0]}->{$temparray->[1]} = [];
+	if (defined($temparray->[2]) && $temparray->[2] =~ m/\d+/) {
+		push(@{$triples->{$temparray->[2]}},[$temparray->[0],$temparray->[1]]);
+	}
 }
 close($fhh);
 for (my $i=0; $i < @{$array}; $i++) {
@@ -30,17 +38,19 @@ for (my $i=0; $i < @{$array}; $i++) {
 	while (my $line = <$fh>) {
 		chomp($line);
 		my $items = [split(/\t/,$line)];
-		$genes->{$items->[0]}->{$items->[8]} = $items;
+		$genes->{$items->[13]}->{$items->[8]} = $items;
 	}
 	foreach my $gene (keys(%{$genes})) {
 		my $genedata = $genes->{$gene};
 		foreach my $cdd (keys(%{$pairhash})) {
 			if (defined($genedata->{$cdd})) {
 				foreach my $cddd (keys(%{$pairhash->{$cdd}})) {
-					my $cdata = $genedata->{$cdd};
-					my $cdatat = $genedata->{$cddd};
-					if ($cdata->[3] > $cdatat->[4] || $cdatat->[3] > $cdata->[4]) {
-						push(@{$pairhash->{$cdd}->{$cddd}},$gene);
+					if (defined($genedata->{$cddd})) {
+						my $cdata = $genedata->{$cdd};
+						my $cdatat = $genedata->{$cddd};
+						if ($cdata->[3] > $cdatat->[4] || $cdatat->[3] > $cdata->[4]) {
+							push(@{$pairhash->{$cdd}->{$cddd}},$gene);
+						}
 					}
 				}
 			}
@@ -54,6 +64,18 @@ foreach my $cdd (keys(%{$pairhash})) {
 	foreach my $cddd (keys(%{$pairhash->{$cdd}})) {
 		print GENELIST $cdd."\t".$cddd."\t".@{$pairhash->{$cdd}->{$cddd}}."\t".join(";",@{$pairhash->{$cdd}->{$cddd}})."\n";
 	}
+}
+foreach my $triple (keys(%{$triples})) {
+	my $genehash = {};
+	my $cddhash = {};
+	for (my $i=0; $i < @{$triples->{$triple}}; $i++) {
+		$cddhash->{$triples->{$triple}->[$i]->[0]} = 1;
+		$cddhash->{$triples->{$triple}->[$i]->[1]} = 1;
+		for (my $j=0; $j < @{$pairhash->{$triples->{$triple}->[$i]->[0]}->{$triples->{$triple}->[$i]->[1]}}; $j++) {
+			$genehash->{$pairhash->{$triples->{$triple}->[$i]->[0]}->{$triples->{$triple}->[$i]->[1]}->[$j]} = 1;
+		}
+	}
+	print GENELIST join("\t",keys(%{$cddhash}))."\t".keys(%{$genehash})."\t".join(";",keys(%{$genehash}))."\n";
 }
 close(GENELIST);
 

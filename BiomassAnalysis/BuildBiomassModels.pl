26825985aec2f2beeb27fea3613b2e7c4dd6090a
Christopher Henry <chenry@mcs.anl.gov>
2016-09-09 02:21:58 -0500

    Fixing biomass build script

--- BiomassAnalysis/BuildBiomassModels.pl
+++ BiomassAnalysis/BuildBiomassModels.pl
@@ -8,6 +8,13 @@ my $procs = $ARGV[0];
 my $index = $ARGV[1];
 
 my $impl = fba_tools::fba_toolsImpl->new();
+
+if (!defined($ENV{'KB_AUTH_TOKEN'})) {
+	require "Bio/KBase/fbaModelServices/ScriptHelpers.pm";
+	$ENV{'KB_AUTH_TOKEN'} = Bio::KBase::fbaModelServices::ScriptHelpers::getToken();
+}
+my $testctx = LocalCallContext->new($ENV{'KB_AUTH_TOKEN'},"chenry",[{'service' => 'fba_tools', 'method' => "unknown", 'method_params' => [{}]}],"unknown");
+$fba_tools::fba_toolsServer::CallContext = $testctx;
 my $ws = $impl->util_kbase_store()->workspace();
 my $genomes = $ws->list_objects({
 	workspaces => ["coremodels"],
@@ -39,4 +46,47 @@ for (my $i=0; $i < @{$genomes}; $i++) {
 		}
 		$count++;
 	}
+}
+
+
+{
+    package LocalCallContext;
+    use strict;
+    sub new {
+        my($class,$token,$user,$provenance,$method) = @_;
+        my $self = {
+            token => $token,
+            user_id => $user,
+            provenance => $provenance,
+            method => $method
+        };
+        return bless $self, $class;
+    }
+    sub user_id {
+        my($self) = @_;
+        return $self->{user_id};
+    }
+    sub token {
+        my($self) = @_;
+        return $self->{token};
+    }
+    sub provenance {
+        my($self) = @_;
+        return $self->{provenance};
+    }
+    sub method {
+        my($self) = @_;
+        return $self->{method};
+    }
+    sub authenticated {
+        return 1;
+    }
+    sub log_debug {
+        my($self,$msg) = @_;
+        print STDERR $msg."\n";
+    }
+    sub log_info {
+        my($self,$msg) = @_;
+        print STDERR $msg."\n";
+    }
 }

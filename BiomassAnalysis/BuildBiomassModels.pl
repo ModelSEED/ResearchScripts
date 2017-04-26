1224935fab76c427d78da2edc167d6d54d9fa4f6
Christopher Henry <chenry@mcs.anl.gov>
2017-04-26 10:55:47 -0500

    Checking in new biomass script to run jobs on twig

--- BiomassAnalysis/BuildBiomassModels.pl
+++ BiomassAnalysis/BuildBiomassModels.pl
@@ -7,19 +7,21 @@ local $| = 1;
 my $procs = $ARGV[0];
 my $index = $ARGV[1];
 
+Bio::KBase::kbaseenv::create_context_from_client_config();
 my $impl = fba_tools::fba_toolsImpl->new();
 
-if (!defined($ENV{'KB_AUTH_TOKEN'})) {
-	require "Bio/KBase/fbaModelServices/ScriptHelpers.pm";
-	$ENV{'KB_AUTH_TOKEN'} = Bio::KBase::fbaModelServices::ScriptHelpers::getToken();
-}
-my $testctx = LocalCallContext->new($ENV{'KB_AUTH_TOKEN'},"chenry",[{'service' => 'fba_tools', 'method' => "unknown", 'method_params' => [{}]}],"unknown");
-$fba_tools::fba_toolsServer::CallContext = $testctx;
-my $ws = $impl->util_kbase_store()->workspace();
-my $genomes = $ws->list_objects({
-	workspaces => ["coremodels"],
+my $initial_genomes = Bio::KBase::kbaseenv::ws_client()->list_objects({
+	workspaces => ["jplfaria:narrative_1492808527866"],
 	type => "KBaseGenomes.Genome",
 });
+my $genomes;
+my $genome_hash = {};
+for (my $i=0; $i < @{$initial_genomes}; $i++) {
+	if ($initial_genomes->[$i]->[1] =~ m/(.+)\.RAST$/) {
+		$genome_hash->{$initial_genomes->[$i]->[1]} = $1;
+		push(@{$genomes},$initial_genomes->[$i]->[1]);
+	}
+}
 
 my $count = 0;
 for (my $i=0; $i < @{$genomes}; $i++) {
@@ -28,64 +30,23 @@ for (my $i=0; $i < @{$genomes}; $i++) {
 			$impl = fba_tools::fba_toolsImpl->new();		}
 		eval {
 			$impl->build_metabolic_model({
-				workspace => "PubSEEDGramNegModels",
-				genome_id => $genomes->[$i]->[1],
-				fbamodel_output_id => $genomes->[$i]->[1].".model",
+				workspace => "chenry:narrative_1493181437626",
+				genome_id => $genomes->[$i],
+				fbamodel_output_id => $genome_hash->{$genomes->[$i]}.".mdl",
 				media_id => "Carbon-D-Glucose",
-				template_id => "gramneg",
-				genome_workspace => "coremodels",
+				template_id => "SuperBiomassTemplate",
+				template_workspace => "chenry:narrative_1493181437626",
+				genome_workspace => "jplfaria:narrative_1492808527866",
 				media_workspace => "KBaseMedia",
 				gapfill_model => 1
 			});
 		};
 		if ($@) {
 			my $error = $@;
-			print $i.":".$genomes->[$i]->[1].":".$error;
+			print $i.":".$genomes->[$i].":".$error;
 		} else {
-			print $i.":".$genomes->[$i]->[1].":success";
+			print $i.":".$genomes->[$i].":success";
 		}
 		$count++;
 	}
-}
-
-{
-    package LocalCallContext;
-    use strict;
-    sub new {
-        my($class,$token,$user,$provenance,$method) = @_;
-        my $self = {
-            token => $token,
-            user_id => $user,
-            provenance => $provenance,
-            method => $method
-        };
-        return bless $self, $class;
-    }
-    sub user_id {
-        my($self) = @_;
-        return $self->{user_id};
-    }
-    sub token {
-        my($self) = @_;
-        return $self->{token};
-    }
-    sub provenance {
-        my($self) = @_;
-        return $self->{provenance};
-    }
-    sub method {
-        my($self) = @_;
-        return $self->{method};
-    }
-    sub authenticated {
-        return 1;
-    }
-    sub log_debug {
-        my($self,$msg) = @_;
-        print STDERR $msg."\n";
-    }
-    sub log_info {
-        my($self,$msg) = @_;
-        print STDERR $msg."\n";
-    }
 }

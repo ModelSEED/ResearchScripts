53e2282ac53ad4a482455615369f918c63b0ae0b
Christopher Henry <chenry@mcs.anl.gov>
2017-06-16 15:47:21 -0500

    Scripts for FBA dump and reaction printing

---
+++ BiomassAnalysis/DumpFBAData.pl
@@ -0,0 +1,172 @@
+use strict;
+use fba_tools::fba_toolsImpl;
+local $| = 1;
+
+my $impl = fba_tools::fba_toolsImpl->new();
+Bio::KBase::kbaseenv::create_context_from_client_config();
+
+my $directory "/disks/p3dev3/ReactionData/";
+my $workspace = "jplfaria:narrative_1493091104031";
+my $initial_models = Bio::KBase::kbaseenv::ws_client()->list_objects({
+	workspaces => [$workspace],
+	type => "KBaseFBA.FBAModel",
+});
+my $models;
+for (my $i=0; $i < @{$initial_models}; $i++) {
+	push(@{$models},$initial_models->[$i]->[1]);
+}
+
+my $rxnhash;
+my $mdlhash;
+my $rxnobjhash;
+for (my $i=0; $i < @{$models}; $i++) {
+	print "Analyzing ".$models->[$i]."\n";
+	my $model = $models->[$i];
+	my $modelobj = $impl->util_store()->get_object($workspace."/".$model);
+	my $rxncount = @{$modelobj->modelreactions()};
+	my $cpdcount = @{$modelobj->modelcompounds()};
+	my $genecount = @{$modelobj->features()};
+	$mdlhash->{$models->[$i]} = {
+		rxn => $rxncount,
+		cpd => $cpdcount,
+		genes => $genecount,
+		gfrxn => 0,
+		ssobj => 0,
+		ssmedia => 0,
+		mmactive => 0,
+		comactive => 0,
+		mmessential => 0,
+		comessential => 0,
+		mmflexible => 0,
+		comflexible => 0,
+		mmblocked => 0,
+		comblocked => 0
+	};
+	my $rxns = $modelobj->modelreactions();
+	for (my $j=0; $j < @{$rxns}; $j++) {
+		$rxnobjhash->{$rxns->[$j]->id()} = $rxns->[$j];
+		if ($rxns->[$j]->gprString() eq "Unknown") {
+			$mdlhash->{$models->[$i]}->{gfrxn}++;
+		}
+		if (length($rxns->[$j]->gapfillString()) == 0) {
+			$mdlhash->{$models->[$i]}->{gfrxn}++;
+		}
+		my $id = $rxns->[$j]->id();
+		$id =~ s/_c0//;
+		if (!defined($rxnhash->{$id})) {
+			$rxnhash->{$id} = {
+				equation => $rxns->[$j]->equation(),
+				definition => $rxns->[$j]->definition(),
+				name => $rxns->[$j]->name(),
+				id => $id
+			};
+		}
+		$rxnhash->{$id}->{$models->[$i]} = {
+			gpr => $rxns->[$j]->gprString(),
+			genelist => $rxns->[$j]->featureIDs(),
+			direction => $rxns->[$j]->direction(),
+			gapfillString => $rxns->[$j]->gapfillString()
+		};
+	}
+	my $fba = $models->[$i].".sensfba";
+	my $fbaobj = $impl->util_store()->get_object($workspace."/".$fba);
+	$mdlhash->{$models->[$i]}->{ssobj} = $fbaobj->objectiveValue();
+	$mdlhash->{$models->[$i]}->{ssmedia} = $fbaobj->media()->_wsname();
+	$rxns = $fbaobj->FBAReactionVariables();
+	for (my $j=0; $j < @{$rxns}; $j++) {
+		my $id = $rxns->[$j]->modelreaction()->id();
+		$id =~ s/_c0//;
+		$rxnhash->{$id}->{$models->[$i]}->{ssflux} = $rxns->[$j]->value();
+		$rxnhash->{$id}->{$models->[$i]}->{biomassdep} = $rxns->[$j]->biomass_dependencies();
+		$rxnhash->{$id}->{$models->[$i]}->{coupled_reactions} = $rxns->[$j]->coupled_reactions();
+	}
+	$fba = $models->[$i].".mmfva";
+	$fbaobj = $impl->util_store()->get_object($workspace."/".$fba);
+	$mdlhash->{$models->[$i]}->{mmobj} = $fbaobj->objectiveValue();
+	$mdlhash->{$models->[$i]}->{mmmedia} = $fbaobj->media()->_wsname();
+	my $cpds = $fbaobj->FBACompoundVariables();
+	for (my $j=0; $j < @{$cpds}; $j++) {
+		if ($cpds->[$j]->value() > 0.0000001 && $cpds->[$j]->modelcompound()->name() =~ m/glucose/i) {
+			$mdlhash->{$models->[$i]}->{uptake}->{$cpds->[$j]->modelcompound()->name()} = $cpds->[$j]->value();
+		} elsif ($cpds->[$j]->value() < -0.0000001 && $cpds->[$j]->modelcompound()->name() =~ m/lactate|formate|butyrate|acetate|succinate|propionate/i) {
+			$mdlhash->{$models->[$i]}->{excretion}->{$cpds->[$j]->modelcompound()->name()} = $cpds->[$j]->value();
+		}
+	}
+	$rxns = $fbaobj->FBAReactionVariables();
+	for (my $j=0; $j < @{$rxns}; $j++) {
+		my $id = $rxns->[$j]->modelreaction()->id();
+		$id =~ s/_c0//;
+		$rxnhash->{$id}->{$models->[$i]}->{mmflux} = $rxns->[$j]->value();
+		$rxnhash->{$id}->{$models->[$i]}->{mmmax} = $rxns->[$j]->max();
+		$rxnhash->{$id}->{$models->[$i]}->{mmmin} = $rxns->[$j]->min();
+		if (abs($rxns->[$j]->value()) > 0.0000001) {
+			$mdlhash->{$models->[$i]}->{mmacitve}++;
+		}
+		if ($rxns->[$j]->min() > 0.0000001) {
+			$mdlhash->{$models->[$i]}->{mmessential}++;
+			$rxnhash->{$id}->{$models->[$i]}->{mmclass} = "p";
+		} elsif ($rxns->[$j]->max() < -0.0000001) {
+			$mdlhash->{$models->[$i]}->{mmessential}++;
+			$rxnhash->{$id}->{$models->[$i]}->{mmclass} = "n";
+		} elsif ($rxns->[$j]->max() > 0.0000001) {
+			if ($rxns->[$j]->min() < -0.0000001) {
+				$mdlhash->{$models->[$i]}->{mmflexible}++;
+				$rxnhash->{$id}->{$models->[$i]}->{mmclass} = "v";
+			} else {
+				$mdlhash->{$models->[$i]}->{mmflexible}++;
+				$rxnhash->{$id}->{$models->[$i]}->{mmclass} = "pv";
+			}
+		} elsif ($rxns->[$j]->min() < -0.0000001) {
+			$mdlhash->{$models->[$i]}->{mmflexible}++;
+			$rxnhash->{$id}->{$models->[$i]}->{mmclass} = "nv";
+		} else {
+			$mdlhash->{$models->[$i]}->{mmblocked}++;
+			$rxnhash->{$id}->{$models->[$i]}->{mmclass} = "b";
+		}
+	}
+	$fba = $models->[$i].".comfva";
+	$fbaobj = $impl->util_store()->get_object($workspace."/".$fba);
+	$mdlhash->{$models->[$i]}->{comobj} = $fbaobj->objectiveValue();
+	$mdlhash->{$models->[$i]}->{commedia} = $fbaobj->media()->_wsname();
+	$rxns = $fbaobj->FBAReactionVariables();
+	for (my $j=0; $j < @{$rxns}; $j++) {
+		my $id = $rxns->[$j]->modelreaction()->id();
+		$id =~ s/_c0//;
+		$rxnhash->{$id}->{$models->[$i]}->{comflux} = $rxns->[$j]->value();
+		$rxnhash->{$id}->{$models->[$i]}->{commax} = $rxns->[$j]->max();
+		$rxnhash->{$id}->{$models->[$i]}->{commin} = $rxns->[$j]->min();
+		if (abs($rxns->[$j]->value()) > 0.0000001) {
+			$mdlhash->{$genomelist->[$i]}->{comacitve}++;
+		}
+		if ($rxns->[$j]->min() > 0.0000001) {
+			$mdlhash->{$models->[$i]}->{comessential}++;
+			$rxnhash->{$id}->{$models->[$i]}->{comclass} = "p";
+		} elsif ($rxns->[$j]->max() < -0.0000001) {
+			$mdlhash->{$models->[$i]}->{comessential}++;
+			$rxnhash->{$id}->{$models->[$i]}->{comclass} = "n";
+		} elsif ($rxns->[$j]->max() > 0.0000001) {
+			if ($rxns->[$j]->min() < -0.0000001) {
+				$mdlhash->{$models->[$i]}->{comflexible}++;
+				$rxnhash->{$id}->{$models->[$i]}->{comclass} = "v";
+			} else {
+				$mdlhash->{$models->[$i]}->{comflexible}++;
+				$rxnhash->{$id}->{$models->[$i]}->{comclass} = "pv";
+			}
+		} elsif ($rxns->[$j]->min() < -0.0000001) {
+			$mdlhash->{$models->[$i]}->{comflexible}++;
+			$rxnhash->{$id}->{$models->[$i]}->{comclass} = "nv";
+		} else {
+			$mdlhash->{$models->[$i]}->{comblocked}++;
+			$rxnhash->{$id}->{$models->[$i]}->{comclass} = "b";
+		}
+	}
+}
+
+my $json = Bio::KBase::ObjectAPI::utilities::TOJSON($rxnhash,1);
+open(my $fout,">",$directory."ModelFBAData.json");
+print $fout $json;
+close($fout);
+$json = Bio::KBase::ObjectAPI::utilities::TOJSON($mdlhash,1);
+open(my $fout,">",$directory."MdlData.json");
+print $fout $json;
+close($fout);
---
+++ BiomassAnalysis/PrintReactionTable.pl
@@ -0,0 +1,193 @@
+#!/usr/bin/perl -w
+
+use strict;
+use fba_tools::fba_toolsImpl;
+$|=1;
+
+my $directory "/disks/p3dev3/ReactionData/";
+my $cpddata = Bio::KBase::ObjectAPI::utilities::FROMJSON(join("\n",@{Bio::KBase::ObjectAPI::utilities::LOADFILE($directory."Compounds.json")}));
+my $globalcpdhash;
+for (my $i=0; $i < @{$cpddata}; $i++) {
+	$globalcpdhash->{$cpddata->[$i]->{id}} = $cpddata->[$i];
+}
+my $rxnmodeldata = Bio::KBase::ObjectAPI::utilities::FROMJSON(join("\n",@{Bio::KBase::ObjectAPI::utilities::LOADFILE($directory."ModelFBAData.json")}));
+
+Bio::KBase::kbaseenv::create_context_from_client_config();
+my $pathway_file = Bio::KBase::ObjectAPI::utilities::LOADFILE($directory."RankedKEGGPathways.txt");
+my $pathwayhash = {};
+my $pathwayarray = [];
+my $pathwayid = {};
+my $forced_pathway_assignments = {};
+for (my $i=1; $i < @{$pathway_file}; $i++) {
+	my $array = [split(/\t/,$pathway_file->[$i])];
+	push(@{$pathwayarray},$array->[1]);
+	$pathwayhash->{$array->[0]} = $array->[1];
+	$pathwayid->{$array->[1]} = $array->[0];
+	for (my $j=3; $j < @{$array}; $j++) {
+		$pathwayhash->{$array->[$j]} = $array->[1];
+	}
+}
+my $rxndata = Bio::KBase::ObjectAPI::utilities::FROMJSON(join("\n",@{Bio::KBase::ObjectAPI::utilities::LOADFILE($directory."Reactions.json")}));
+my $rxndatahash = {};
+for (my $i=0; $i < @{$rxndata}; $i++) {
+	$rxndatahash->{$rxndata->[$i]->{id}} = $rxndata->[$i];
+}
+#Print the reactions, organized and prioritized by pathway
+open(my $fout,">",$directory."ModelReactions.txt");
+print $fout "Pathway\tReaction\tEquation\tReversibility\tStatus\tKEGG ID\tMetaCyc ID\tName\tEC numbers\tRoles";
+my $pathwaymodelhash;
+for (my $i=0; $i < @{$genomelist}; $i++) {
+	print $fout "\t".$genomelist->[$i];
+}
+print $fout "\n";
+my $donerxn = {};
+for (my $i=0; $i < @{$pathwayarray}; $i++) {
+	 my $list = [];
+	 foreach my $rxn (keys(%{$rxnmodeldata})) {
+	 	$rxn =~ s/_c0//;
+	 	if (!defined($donerxn->{$rxn})) {
+	 		if (defined($forced_pathway_assignments->{$rxn})) {
+	 			if ($forced_pathway_assignments->{$rxn} eq $pathwayarray->[$i]) {
+	 				$donerxn->{$rxn} = 1;
+					(my $line,my $priority) = &print_reaction($rxn,$pathwayarray->[$i]);
+					push(@{$list},{line => $line,priority => $priority});
+	 			}
+	 		} elsif (defined($rxndatahash->{$rxn})) {
+				if (defined($rxndatahash->{$rxn}->{kegg_pathways})) {
+					for (my $j=0; $j < @{$rxndatahash->{$rxn}->{kegg_pathways}}; $j++) {
+						if (defined($pathwayhash->{$rxndatahash->{$rxn}->{kegg_pathways}->[$j]})) {
+							if ($pathwayhash->{$rxndatahash->{$rxn}->{kegg_pathways}->[$j]} eq $pathwayarray->[$i]) {
+								$donerxn->{$rxn} = 1;
+								(my $line,my $priority) = &print_reaction($rxn,$pathwayarray->[$i]);
+								push(@{$list},{line => $line,priority => $priority});;
+							}
+						}
+					}
+				} elsif (defined($rxndatahash->{$rxn}->{metacyc_pathways})) {
+					for (my $j=0; $j < @{$rxndatahash->{$rxn}->{metacyc_pathways}}; $j++) {
+						if (defined($pathwayhash->{$rxndatahash->{$rxn}->{metacyc_pathways}->[$j]})) {
+							if ($pathwayhash->{$rxndatahash->{$rxn}->{metacyc_pathways}->[$j]} eq $pathwayarray->[$i]) {
+								$donerxn->{$rxn} = 1;
+								(my $line,my $priority) = &print_reaction($rxn,$pathwayarray->[$i]);
+								push(@{$list},{line => $line,priority => $priority});
+							}
+						}
+					}
+				}
+			}
+	 	}
+	}
+	my $sortedlines = [sort { $b->{priority} <=> $a->{priority} } @{$list} ];
+	for (my $j=0; $j < @{$sortedlines}; $j++) {
+		print $fout $sortedlines->[$j]->{line}."\n";
+	}
+}
+my $list;
+foreach my $rxn (keys(%{$rxnmodeldata})) {
+	$rxn =~ s/_c0//;
+	if (!defined($donerxn->{$rxn})) {
+		(my $line,my $priority) = &print_reaction($rxn);
+		push(@{$list},{line => $line,priority => $priority});
+	}
+}
+my $sortedlines = [sort { $b->{priority} <=> $a->{priority} } @{$list} ];
+for (my $i=0; $i < @{$sortedlines}; $i++) {
+	print $fout $sortedlines->[$i]->{line}."\n";
+}
+close($fout);
+
+sub print_reaction {
+	my ($id,$pathway) = @_;
+	#Printing general reaction data
+	my $line = "";
+	if (defined($pathway)) {
+		$line .= $pathway."\t".$id."\t";
+	} else {
+		$line .= "Unassigned\t".$id."\t";
+	}
+	if (defined($rxndatahash->{$id})) {
+		my $rxnobj = $rxndatahash->{$id};
+		my $headings = [
+			["definition",0],
+			["reversibility",0],
+			["status",0],
+			["kegg_aliases",1],
+			["metacyc_aliases",1],
+			["names",1],
+			["ec_numbers",1],
+			["roles",2,";"]
+		];
+		for (my $k=0; $k < @{$headings}; $k++) {
+			if (defined($rxnobj->{$headings->[$k]->[0]})) {
+				if ($headings->[$k]->[1] == 0) {
+					$line .= $rxnobj->{$headings->[$k]->[0]};
+				} elsif ($headings->[$k]->[1] == 1) {
+					$line .= join("|",@{$rxnobj->{$headings->[$k]->[0]}});
+				} else {
+					my $subarray = [];
+					for (my $m=0; $m < @{$rxnobj->{$headings->[$k]->[0]}}; $m++) {
+						my $miniarray = [split(/;/,$rxnobj->{$headings->[$k]->[0]}->[$m])];
+						push(@{$subarray},$miniarray->[1]);
+					}
+					$line .= join("|",@{$subarray});
+				}
+			}
+			$line .= "\t";
+		}
+	} else {
+		$line .= "\t\t\t\t\t\t\t\t\t";
+	}
+	#Now printing strain specific data
+	my $priority = 0;
+	my $rxnobj = $rxnmodeldata->{$id};
+	for (my $i=0; $i < @{$genomelist}; $i++) {
+		if ($i > 0) {
+			$line .= "\t";
+		}
+		if (defined($rxnobj->{$genomelist->[$i]})) {
+			my $rxngdata = $rxnobj->{$genomelist->[$i]};
+			if (length($rxngdata->{gapfillString}) > 0) {
+				$line .= "G/";
+			}
+			if ($rxngdata->{ssflux} > 0) {
+				$line .= "PosF/";
+			} elsif ($rxngdata->{ssflux} < 0) {
+				$line .= "NegF/";
+			} else{
+				$line .= "NoF/";
+			}
+			$line .= $rxngdata->{mmclass}."/".$rxngdata->{comclass}."/".$rxngdata->{direction}."/";
+			if (defined($rxngdata->{biomassdep})) {
+				for (my $j=0; $j < @{$rxngdata->{biomassdep}};  $j++) {
+					if ($j > 0) {
+						$line .= ";";
+					}
+					my $cpdid = $rxngdata->{biomassdep}->[$j]->[1];
+					$cpdid =~ s/_c0//;
+					$line .= $globalcpdhash->{$cpdid}->{name}; 
+				}
+				$line .= "/";
+			}
+			my $genelists = "";
+			if (defined($rxngdata->{genelist})) {
+				$genelists = join(",",@{$rxngdata->{genelist}});
+			}
+			$genelists =~ s/fig\|\d+\.\d+\.peg\.//g;
+			$line .= $genelists."/";
+			if ($rxngdata->{ssflux} != 0) {
+				$priority += 10;
+			} elsif ($rxngdata->{mmclass} eq "p" && $rxngdata->{mmclass} eq "n") {
+				$priority += 8;
+			} elsif ($rxngdata->{mmclass} ne "b") {
+				$priority += 6;
+			} elsif ($rxngdata->{comclass} ne "b") {
+				$priority += 4;
+			} elsif (length($rxngdata->{gapfillString}) > 0) {
+				$priority += 2;
+			}
+		} else {
+			$line .= "-";
+		}
+	}
+	return ($line,$priority);
+}

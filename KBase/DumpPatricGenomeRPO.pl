7610e5f3118b00decbef65959fa3d7e638aaacd2
Christopher Henry <chenry@mcs.anl.gov>
2018-02-01 03:24:21 -0600

    Fixing RPOB script

--- KBase/DumpPatricGenomeRPO.pl
+++ KBase/DumpPatricGenomeRPO.pl
@@ -25,7 +25,7 @@ my(@fields) = qw(feature_type
 		 
 my @res = $d->query("genome_feature",
 	["select", @fields],
-	["eq", "feature_type", "rRNA"],
+	["eq", "feature_type", "CDS"],
 	["eq", "annotation", "PATRIC"],
 	["eq", "product", "DNA-directed RNA polymerase beta subunit (EC 2.7.7.6)"],
 );
@@ -43,7 +43,7 @@ print "Now retrieving rpoC!\n";
 
 @res = $d->query("genome_feature",
 	["select", @fields],
-	["eq", "feature_type", "rRNA"],
+	["eq", "feature_type", "CDS"],
 	["eq", "annotation", "PATRIC"],
 	["eq", "product", "DNA-directed RNA polymerase beta' subunit (EC 2.7.7.6)"],
 );

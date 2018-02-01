793e876b6f8ed6594b6233ed7471b76607e329ff
Christopher Henry <chenry@mcs.anl.gov>
2018-02-01 03:21:01 -0600

    Fixing RPO dump

--- KBase/DumpPatricGenomeRPO.pl
+++ KBase/DumpPatricGenomeRPO.pl
@@ -23,14 +23,14 @@ my(@fields) = qw(feature_type
 	product
 );
 		 
-@res = $d->query("genome_feature",
+my @res = $d->query("genome_feature",
 	["select", @fields],
 	["eq", "feature_type", "rRNA"],
 	["eq", "annotation", "PATRIC"],
 	["eq", "product", "DNA-directed RNA polymerase beta subunit (EC 2.7.7.6)"],
 );
 
-for (my $i=0; $i < $count; $i++) {
+for (my $i=0; $i < @res; $i++) {
 	if ($res[$i]->{patric_id} =~ m/fig\|(\d+\.\d+)\./) {
 		my $genomeid = $1;
 		if (defined($output->{$genomeid})) {
@@ -48,7 +48,7 @@ print "Now retrieving rpoC!\n";
 	["eq", "product", "DNA-directed RNA polymerase beta' subunit (EC 2.7.7.6)"],
 );
 
-for (my $i=0; $i < $count; $i++) {
+for (my $i=0; $i < @res; $i++) {
 	if ($res[$i]->{patric_id} =~ m/fig\|(\d+\.\d+)\./) {
 		my $genomeid = $1;
 		if (defined($output->{$genomeid})) {
@@ -57,4 +57,4 @@ for (my $i=0; $i < $count; $i++) {
 	}
 }
 
-Bio::KBase::ObjectAPI::utilities::PRINTFILE($filename,[Bio::KBase::ObjectAPI::utilities::TOJSON($path."/AllPatricGenomes2.txt",1)]);
+Bio::KBase::ObjectAPI::utilities::PRINTFILE($path."/AllPatricGenomes2.txt",[Bio::KBase::ObjectAPI::utilities::TOJSON($output,1)]);

8019b28c465dd08ae135d1d7d02008d1503ebccf
Christopher Henry <chenry@mcs.anl.gov>
2020-02-17 10:56:36 -0600

    Updating mag code

--- KBase/ProcessMAGGenome.pl
+++ KBase/ProcessMAGGenome.pl
@@ -12,111 +12,119 @@ my $impl = fba_tools::fba_toolsImpl->new();
 Bio::KBase::kbaseenv::create_context_from_client_config();
 Bio::KBase::ObjectAPI::functions::set_handler($impl);
 
-my $rxnhash = Bio::KBase::utilities::reaction_hash();
+my $datachannel = {};
+Bio::KBase::ObjectAPI::functions::func_model_based_genome_characterization({
+	workspace => $workspace,
+	genome_id => $genome_id,
+	genome_workspace => $workspace,
+	gapfill_model => 0,
+},$datachannel);
 
-if ($genome_id =~ m/(.+)\.RAST$/) {
-	my $name = $1;
-	print "Now processing:".$genome_id."\n";
-	my $datachannel = {};
-	#Building base model which populates base ATP characteristics
-	Bio::KBase::ObjectAPI::functions::func_build_metabolic_model({
-		workspace => $workspace,
-		genome_id => $genome_id,
-		fbamodel_output_id => $name.".base",
-		genome_workspace => $workspace,
-		gapfill_model => 0,
-	},$datachannel);
-	my $auxo_output = Bio::KBase::ObjectAPI::functions::func_predict_auxotrophy_from_model({
-		workspace => $workspace,
-		fbamodel_id => $name.".base",
-	},$datachannel);
-	my $baseline_gapfilling = $datachannel->{fbamodel}->attributes()->{baseline_gapfilling};
-	delete $datachannel->{fbamodel};
-	my $gapfill_output = Bio::KBase::ObjectAPI::functions::func_gapfill_metabolic_model({
-		workspace => $workspace,
-		fbamodel_id => $name.".base",
-		media_id => $name.".base".".auxo_media",
-		fbamodel_output_id => $name.".gapfilled"
-	},$datachannel);
-	$datachannel->{fbamodel}->attributes()->{baseline_gapfilling} = $baseline_gapfilling;
-	$datachannel->{fbamodel}->attributes()->{auxotrophy_gapfilling} = $gapfill_output->{number_gapfilled_reactions};
-	$datachannel->{fbamodel}->attributes()->{gapfilled_atpprod} = $gapfill_output->{atpproduction};
-	$datachannel->{fbamodel}->attributes()->{gapfilled_biomass} = $gapfill_output->{growth};
-	my $fba_output = Bio::KBase::ObjectAPI::functions::func_run_flux_balance_analysis({
-		workspace => $workspace,
-		fbamodel_id => $name.".gapfilled",
-		fba_output_id => $name.".fba",
-		media_id => $name.".base".".auxo_media",
-		fva => 1,
-		minimize_flux => 1,
-		max_c_uptake => 30
-	},$datachannel);	
-	$datachannel->{fbamodel}->attributes()->{auxo_biomass} = $fba_output->{objective};
-	my $fbaobj = $datachannel->{fba};
-	my $rxnvar = $fbaobj->FBAReactionVariables();
-	my $classhash = {};
-	for (my $i=0; $i < @{$rxnvar}; $i++) {
-		if ($rxnvar->[$i]->modelreaction_ref() =~ m/(rxn\d+)/) {
-			$classhash->{$1}->{auxo} = $rxnvar->[$i]->{class};
-		}
-		if (!defined($datachannel->{fbamodel}->attributes()->{"auxo_class_".$rxnvar->[$i]->{class}})) {
-			$datachannel->{fbamodel}->attributes()->{"auxo_class_".$rxnvar->[$i]->{class}} = 0;
-		}
-		$datachannel->{fbamodel}->attributes()->{"auxo_class_".$rxnvar->[$i]->{class}}++;
-	}
-	$fba_output = Bio::KBase::ObjectAPI::functions::func_run_flux_balance_analysis({
-		workspace => $workspace,
-		fbamodel_id => $name.".gapfilled",
-		fba_output_id => $name.".fba",
-		fva => 1,
-		minimize_flux => 1,
-		max_c_uptake => 30
-	},$datachannel);	
-	$datachannel->{fbamodel}->attributes()->{complete_biomass} = $fba_output->{objective};
-	my $fbaobj = $datachannel->{fba};
-	my $rxnvar = $fbaobj->FBAReactionVariables();
-	for (my $i=0; $i < @{$rxnvar}; $i++) {
-		if ($rxnvar->[$i]->modelreaction_ref() =~ m/(rxn\d+)/) {
-			$classhash->{$1}->{comp} = $rxnvar->[$i]->{class};
-		}
-		if (!defined($datachannel->{fbamodel}->attributes()->{"complete_class_".$rxnvar->[$i]->{class}})) {
-			$datachannel->{fbamodel}->attributes()->{"complete_class_".$rxnvar->[$i]->{class}} = 0;
-		}
-		$datachannel->{fbamodel}->attributes()->{"complete_class_".$rxnvar->[$i]->{class}}++;
-	}
-	my $rxns = $datachannel->{fbamodel}->modelreactions();
-	for (my $i=0; $i < @{$rxns}; $i++) {
-		if ($rxns->[$i]->id() =~ m/(rxn\d+)/) {
-			my $rxnid = $1;
-			if (defined($rxnhash->{$rxnid}) && defined($rxnhash->{$rxnid}->{kegg_pathways})) {
-				for (my $j=0; $j < @{$rxnhash->{$rxnid}->{kegg_pathways}}; $j++) {
-					if (!defined($datachannel->{fbamodel}->attributes()->{"pathways_".$rxnhash->{$rxnid}->{kegg_pathways}->[$j]})) {
-						$datachannel->{fbamodel}->attributes()->{"pathways_".$rxnhash->{$rxnid}->{kegg_pathways}->[$j]} = {
-							rxn => 0, nonblocked => 0, gf => 0,rxns => {}
-						};
-					}
-					if (length($rxns->[$i]->gapfillString()) > 0) {
-						$datachannel->{fbamodel}->attributes()->{"pathways_".$rxnhash->{$rxnid}->{kegg_pathways}->[$j]}->{gf}++;
-						$datachannel->{fbamodel}->attributes()->{"pathways_".$rxnhash->{$rxnid}->{kegg_pathways}->[$j]}->{rxns}->{$rxnid} = "g";
-					} else {
-						$datachannel->{fbamodel}->attributes()->{"pathways_".$rxnhash->{$rxnid}->{kegg_pathways}->[$j]}->{rxn}++;
-						$datachannel->{fbamodel}->attributes()->{"pathways_".$rxnhash->{$rxnid}->{kegg_pathways}->[$j]}->{rxns}->{$rxnid} = "n";
-					}
-					if (defined($classhash->{$rxnid}->{comp}) && $classhash->{$rxnid}->{comp} ne "Blocked") {
-						$datachannel->{fbamodel}->attributes()->{"pathways_".$rxnhash->{$rxnid}->{kegg_pathways}->[$j]}->{nonblocked}++;
-						if ($datachannel->{fbamodel}->attributes()->{"pathways_".$rxnhash->{$rxnid}->{kegg_pathways}->[$j]}->{rxns}->{$rxnid} ne "g") {
-							$datachannel->{fbamodel}->attributes()->{"pathways_".$rxnhash->{$rxnid}->{kegg_pathways}->[$j]}->{rxns}->{$rxnid} = "a";
-						}
-					}
-				}
-			}
-		}
-	}
-	foreach my $cpd (keys(%{$auxo_output->{auxotrophy_data}})) {
-		$datachannel->{fbamodel}->attributes()->{"auxotrophy_".$cpd} = $auxo_output->{auxotrophy_data}->{$cpd}->{name}.":".
-			$auxo_output->{auxotrophy_data}->{$cpd}->{totalrxn}.":".
-			$auxo_output->{auxotrophy_data}->{$cpd}->{gfrxn}.":".
-			$auxo_output->{auxotrophy_data}->{$cpd}->{auxotrophic};
-	}
-	my $wsmeta = $impl->util_save_object($datachannel->{fbamodel},Bio::KBase::utilities::buildref($name.".gapfilled",$workspace));
-}
+#my $rxnhash = Bio::KBase::utilities::reaction_hash();
+#
+#if ($genome_id =~ m/(.+)\.RAST$/) {
+#	my $name = $1;
+#	print "Now processing:".$genome_id."\n";
+#	my $datachannel = {};
+#	#Building base model which populates base ATP characteristics
+#	Bio::KBase::ObjectAPI::functions::func_build_metabolic_model({
+#		workspace => $workspace,
+#		genome_id => $genome_id,
+#		fbamodel_output_id => $name.".base",
+#		genome_workspace => $workspace,
+#		gapfill_model => 0,
+#	},$datachannel);
+#	my $auxo_output = Bio::KBase::ObjectAPI::functions::func_predict_auxotrophy_from_model({
+#		workspace => $workspace,
+#		fbamodel_id => $name.".base",
+#	},$datachannel);
+#	my $baseline_gapfilling = $datachannel->{fbamodel}->attributes()->{baseline_gapfilling};
+#	delete $datachannel->{fbamodel};
+#	my $gapfill_output = Bio::KBase::ObjectAPI::functions::func_gapfill_metabolic_model({
+#		workspace => $workspace,
+#		fbamodel_id => $name.".base",
+#		media_id => $name.".base".".auxo_media",
+#		fbamodel_output_id => $name.".gapfilled"
+#	},$datachannel);
+#	$datachannel->{fbamodel}->attributes()->{baseline_gapfilling} = $baseline_gapfilling;
+#	$datachannel->{fbamodel}->attributes()->{auxotrophy_gapfilling} = $gapfill_output->{number_gapfilled_reactions};
+#	$datachannel->{fbamodel}->attributes()->{gapfilled_atpprod} = $gapfill_output->{atpproduction};
+#	$datachannel->{fbamodel}->attributes()->{gapfilled_biomass} = $gapfill_output->{growth};
+#	my $fba_output = Bio::KBase::ObjectAPI::functions::func_run_flux_balance_analysis({
+#		workspace => $workspace,
+#		fbamodel_id => $name.".gapfilled",
+#		fba_output_id => $name.".fba",
+#		media_id => $name.".base".".auxo_media",
+#		fva => 1,
+#		minimize_flux => 1,
+#		max_c_uptake => 30
+#	},$datachannel);	
+#	$datachannel->{fbamodel}->attributes()->{auxo_biomass} = $fba_output->{objective};
+#	my $fbaobj = $datachannel->{fba};
+#	my $rxnvar = $fbaobj->FBAReactionVariables();
+#	my $classhash = {};
+#	for (my $i=0; $i < @{$rxnvar}; $i++) {
+#		if ($rxnvar->[$i]->modelreaction_ref() =~ m/(rxn\d+)/) {
+#			$classhash->{$1}->{auxo} = $rxnvar->[$i]->{class};
+#		}
+#		if (!defined($datachannel->{fbamodel}->attributes()->{"auxo_class_".$rxnvar->[$i]->{class}})) {
+#			$datachannel->{fbamodel}->attributes()->{"auxo_class_".$rxnvar->[$i]->{class}} = 0;
+#		}
+#		$datachannel->{fbamodel}->attributes()->{"auxo_class_".$rxnvar->[$i]->{class}}++;
+#	}
+#	$fba_output = Bio::KBase::ObjectAPI::functions::func_run_flux_balance_analysis({
+#		workspace => $workspace,
+#		fbamodel_id => $name.".gapfilled",
+#		fba_output_id => $name.".fba",
+#		fva => 1,
+#		minimize_flux => 1,
+#		max_c_uptake => 30
+#	},$datachannel);	
+#	$datachannel->{fbamodel}->attributes()->{complete_biomass} = $fba_output->{objective};
+#	my $fbaobj = $datachannel->{fba};
+#	my $rxnvar = $fbaobj->FBAReactionVariables();
+#	for (my $i=0; $i < @{$rxnvar}; $i++) {
+#		if ($rxnvar->[$i]->modelreaction_ref() =~ m/(rxn\d+)/) {
+#			$classhash->{$1}->{comp} = $rxnvar->[$i]->{class};
+#		}
+#		if (!defined($datachannel->{fbamodel}->attributes()->{"complete_class_".$rxnvar->[$i]->{class}})) {
+#			$datachannel->{fbamodel}->attributes()->{"complete_class_".$rxnvar->[$i]->{class}} = 0;
+#		}
+#		$datachannel->{fbamodel}->attributes()->{"complete_class_".$rxnvar->[$i]->{class}}++;
+#	}
+#	my $rxns = $datachannel->{fbamodel}->modelreactions();
+#	for (my $i=0; $i < @{$rxns}; $i++) {
+#		if ($rxns->[$i]->id() =~ m/(rxn\d+)/) {
+#			my $rxnid = $1;
+#			if (defined($rxnhash->{$rxnid}) && defined($rxnhash->{$rxnid}->{kegg_pathways})) {
+#				for (my $j=0; $j < @{$rxnhash->{$rxnid}->{kegg_pathways}}; $j++) {
+#					if (!defined($datachannel->{fbamodel}->attributes()->{"pathways_".$rxnhash->{$rxnid}->{kegg_pathways}->[$j]})) {
+#						$datachannel->{fbamodel}->attributes()->{"pathways_".$rxnhash->{$rxnid}->{kegg_pathways}->[$j]} = {
+#							rxn => 0, nonblocked => 0, gf => 0,rxns => {}
+#						};
+#					}
+#					if (length($rxns->[$i]->gapfillString()) > 0) {
+#						$datachannel->{fbamodel}->attributes()->{"pathways_".$rxnhash->{$rxnid}->{kegg_pathways}->[$j]}->{gf}++;
+#						$datachannel->{fbamodel}->attributes()->{"pathways_".$rxnhash->{$rxnid}->{kegg_pathways}->[$j]}->{rxns}->{$rxnid} = "g";
+#					} else {
+#						$datachannel->{fbamodel}->attributes()->{"pathways_".$rxnhash->{$rxnid}->{kegg_pathways}->[$j]}->{rxn}++;
+#						$datachannel->{fbamodel}->attributes()->{"pathways_".$rxnhash->{$rxnid}->{kegg_pathways}->[$j]}->{rxns}->{$rxnid} = "n";
+#					}
+#					if (defined($classhash->{$rxnid}->{comp}) && $classhash->{$rxnid}->{comp} ne "Blocked") {
+#						$datachannel->{fbamodel}->attributes()->{"pathways_".$rxnhash->{$rxnid}->{kegg_pathways}->[$j]}->{nonblocked}++;
+#						if ($datachannel->{fbamodel}->attributes()->{"pathways_".$rxnhash->{$rxnid}->{kegg_pathways}->[$j]}->{rxns}->{$rxnid} ne "g") {
+#							$datachannel->{fbamodel}->attributes()->{"pathways_".$rxnhash->{$rxnid}->{kegg_pathways}->[$j]}->{rxns}->{$rxnid} = "a";
+#						}
+#					}
+#				}
+#			}
+#		}
+#	}
+#	foreach my $cpd (keys(%{$auxo_output->{auxotrophy_data}})) {
+#		$datachannel->{fbamodel}->attributes()->{"auxotrophy_".$cpd} = $auxo_output->{auxotrophy_data}->{$cpd}->{name}.":".
+#			$auxo_output->{auxotrophy_data}->{$cpd}->{totalrxn}.":".
+#			$auxo_output->{auxotrophy_data}->{$cpd}->{gfrxn}.":".
+#			$auxo_output->{auxotrophy_data}->{$cpd}->{auxotrophic};
+#	}
+#	my $wsmeta = $impl->util_save_object($datachannel->{fbamodel},Bio::KBase::utilities::buildref($name.".gapfilled",$workspace));
+#}
--- KBase/ProcessMAGGenomes.pl
+++ KBase/ProcessMAGGenomes.pl
@@ -6,6 +6,8 @@ use fba_tools::fba_toolsImpl;
 local $| = 1;
 
 my $workspace = $ARGV[0];
+my $index = $ARGV[1];
+my $processes = $ARGV[2];
 
 my $impl = fba_tools::fba_toolsImpl->new();
 Bio::KBase::kbaseenv::create_context_from_client_config();
@@ -17,8 +19,14 @@ my $output = Bio::KBase::kbaseenv::list_objects({
 
 my $rxnhash = Bio::KBase::utilities::reaction_hash();
 
+my $modellist = [];
 for (my $i=0; $i < @{$output}; $i++) {
-	if ($output->[$i]->[2] =~ m/KBaseGenomes\.Genome/ && $output->[$i]->[1] =~ m/(.+)\.RAST$/) {
+	if ($output->[$i]->[2] =~ m/KBaseGenomes\.Genome/ && $output->[$i]->[1] =~ m/RAST2/) {
+		push(@{$modellist},$output->[$i]->[1]);
+	}
+}
+for (my $i=0; $i < @{$modellist}; $i++) {
+	if ($i % $processes == $index) {
 		system("perl ProcessMAGGenome.pl ".$output->[$i]->[1]." ".$workspace);
 	}
 }

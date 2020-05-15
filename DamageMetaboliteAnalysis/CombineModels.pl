f4ee0cebfd356ec6ccd3be86d78f4c7040a6a210
Christopher Henry <chenry@mcs.anl.gov>
2020-05-15 08:50:04 -0500

    Checking in local files to enable git pull

--- DamageMetaboliteAnalysis/CombineModels.pl
+++ DamageMetaboliteAnalysis/CombineModels.pl
@@ -15,15 +15,10 @@ my $rep_model = $impl->util_get_object(Bio::KBase::utilities::buildref("MMSyn3_r
 my $old_dam_model = $impl->util_get_object(Bio::KBase::utilities::buildref("iMB155.damage-deleted-1572588332028",$workspace));
 my $old_rep_model = $impl->util_get_object(Bio::KBase::utilities::buildref("iMB155.repair-deleted-1572588335426",$workspace));
 
-my $catagories = {
-	cpdid => 0,
-	metabolite_hit => 0,
-	rejected => 0,
-	extracellular => 0,
-	damage_metabolite_hit => 0,
-	cpd_metabolite_hit => 0,
-	model_metabolite_hit => 0
-};
+my $peakhits = {};
+my $cpdhits = {};
+my $otherhits = {};
+my $totalcpds = {};
 
 my $model_cpd_ids = {};
 my $cpds = $model->modelcompounds();
@@ -39,25 +34,49 @@ my $metabolomics_hash = {};
 for (my $i=0; $i < @{$lines}; $i++) {
 	my $array = [split(/[\t\s]/,$lines->[$i])];
 	my $subarray = [split(/[_-]/,$array->[0])];
-	$metabolomics_hash->{$subarray->[0]} = 1;
+	$metabolomics_hash->{$subarray->[0]}->{"peak.".($i+1)} = 1;
 }
 
 #Building structure hashes
 my $hash = {};
 my $cpds = $model->modelcompounds();
 for (my $i=0; $i < @{$cpds}; $i++) {
+	$totalcpds->{$cpds->[$i]->id()} = 1;
 	my $array = [split(/[_-]/,$cpds->[$i]->inchikey())];
 	if (defined($metabolomics_hash->{$array->[0]})) {
-		$catagories->{model_metabolite_hit}++;
+		foreach my $peakid (keys(%{$metabolomics_hash->{$array->[0]}})) {
+			$peakhits->{all}->{$peakid}->{$cpds->[$i]->id()} = 1;
+			$peakhits->{model}->{$peakid}->{$cpds->[$i]->id()} = 1;
+			if ($cpds->[$i]->id() =~ m/cpd\d+/) {
+				$cpdhits->{model}->{$cpds->[$i]->id()}->{$peakid} = 1;
+				$cpdhits->{all}->{$cpds->[$i]->id()}->{$peakid} = 1;
+			} else {
+				$otherhits->{model}->{$cpds->[$i]->id()}->{$peakid} = 1;
+				$otherhits->{all}->{$cpds->[$i]->id()}->{$peakid} = 1;
+			}
+		}
 	}
 	$hash->{model}->{inchikey}->{$cpds->[$i]->inchikey()} = $cpds->[$i];
 	$hash->{model}->{base}->{$array->[0]}->{$cpds->[$i]->id()} = $cpds->[$i];
 }
+my $rxns = $model->modelreactions();
+
+print "Total model cpd:".@{$cpds}."\n";
+print "Total model rxn:".@{$rxns}."\n";
+print "Model peaks:".keys(%{$peakhits->{model}})."\n";
+print "Model seed compounds:".keys(%{$cpdhits->{model}})."\n";
+print "Model other compounds:".keys(%{$otherhits->{model}})."\n";
+
 my $cpd_hash = Bio::KBase::utilities::compound_hash();
 foreach my $cpd (keys(%{$cpd_hash})) {
 	$cpd_hash->{reaction_count} = 0;
 	if (defined($cpd_hash->{$cpd}->{inchikey})) {
 		my $array = [split(/[_-]/,$cpd_hash->{$cpd}->{inchikey})];
+		if (defined($metabolomics_hash->{$array->[0]})) {
+			foreach my $peakid (keys(%{$metabolomics_hash->{$array->[0]}})) {
+				$cpdhits->{db}->{$cpd_hash->{$cpd}->{id}."_c0"}->{$peakid} = 1;
+			}
+		}
 		$hash->{db}->{inchikey}->{$cpd_hash->{$cpd}->{inchikey}} = $cpd_hash->{$cpd};
 		$hash->{db}->{base}->{$array->[0]}->{$cpd_hash->{$cpd}->{id}} = $cpd_hash->{$cpd};
 	}
@@ -73,9 +92,6 @@ foreach my $rxn (keys(%{$rxn_hash})) {
 $cpds = $dam_model->modelcompounds();
 for (my $i=0; $i < @{$cpds}; $i++) {
 	my $array = [split(/[_-]/,$cpds->[$i]->inchikey())];
-	if (defined($metabolomics_hash->{$array->[0]})) {
-		$catagories->{damage_metabolite_hit}++;
-	}
 	$hash->{dam}->{inchikey}->{$cpds->[$i]->inchikey()} = $cpds->[$i];
 	$hash->{dam}->{base}->{$array->[0]}->{$cpds->[$i]->id()} = $cpds->[$i];
 }
@@ -122,51 +138,65 @@ my $translation = {};
 my $cpds = $dam_model->modelcompounds();
 my $model_cpd_hash;
 for (my $i=0; $i < @{$cpds}; $i++) {
-	if ($cpds->[$i]->id() =~ m/pkc/ && $cpds->[$i]->id() !~ m/_e0/ && $cpds->[$i]->inchikey() ne "none") {
+	if (length($cpds->[$i]->inchikey()) > 0 && $cpds->[$i]->inchikey() ne "none") {
 		my $array = [split(/[_-]/,$cpds->[$i]->inchikey())];
-		if (defined($hash->{model}->{inchikey}->{$cpds->[$i]->inchikey()})) {
-			my $cpddata = $hash->{model}->{inchikey}->{$cpds->[$i]->inchikey()};
-			print $cpds->[$i]->id().":missed full match:".$cpddata->id().":".$cpddata->name().":model\n";
-			$translation->{$cpds->[$i]->id()} = $cpddata->id();
-			$cpds->[$i]->id($cpddata->id());
-		} elsif (defined($hash->{db}->{inchikey}->{$cpds->[$i]->inchikey()})) {
-			my $cpddata = $hash->{db}->{inchikey}->{$cpds->[$i]->inchikey()};
-			print $cpds->[$i]->id().":missed full match:".$cpddata->{id}.":".$cpddata->{name}.":db\n";
-			$translation->{$cpds->[$i]->id()} = $cpddata->{id}."_c0";
-			$cpds->[$i]->id($cpddata->{id}."_c0");
-		} elsif (defined($hash->{model}->{base}->{$array->[0]})) {
-			my $selected;
-			foreach my $newcpd (keys(%{$hash->{model}->{base}->{$array->[0]}})) {
-				my $cpddata = $hash->{model}->{base}->{$array->[0]}->{$newcpd};
-				if (!defined($selected) || $selected->id() =~ m/_e0$/) {
-					$selected = $cpddata;
-				}
-				print $cpds->[$i]->id().":missed base match:".$cpddata->id().":".$cpddata->name().":model\n";
-			}
-			print $cpds->[$i]->id().":selected missed base match:".$selected->id().":".$selected->name().":model\n";
-			$model_cpd_hash->{$cpds->[$i]->id()} = $cpds->[$i];
-			$translation->{$cpds->[$i]->id()} = $selected->id();
-			$cpds->[$i]->id($selected->id());
-		} elsif (defined($hash->{db}->{base}->{$array->[0]})) {
-			my $selected;
-			foreach my $newcpd (keys(%{$hash->{db}->{base}->{$array->[0]}})) {
-				my $cpddata = $hash->{db}->{base}->{$array->[0]}->{$newcpd};
-				if (!defined($selected) || $cpddata->{reaction_count} >= $selected->{reaction_count}) {
-					if (!defined($selected)) {
-						$selected = $cpddata;				
-					} elsif ($cpddata->{reaction_count} == $selected->{reaction_count}) {
-						if ($cpddata->{id} < $selected->{id}) {
-							$selected = $cpddata;
-						}
-					} else {
+		if ($cpds->[$i]->id() =~ m/pkc/ && $cpds->[$i]->id() !~ m/_e0/ && $cpds->[$i]->inchikey() ne "none") {
+			if (defined($hash->{model}->{inchikey}->{$cpds->[$i]->inchikey()})) {
+				my $cpddata = $hash->{model}->{inchikey}->{$cpds->[$i]->inchikey()};
+				#print $cpds->[$i]->id().":missed full match:".$cpddata->id().":".$cpddata->name().":model\n";
+				$translation->{$cpds->[$i]->id()} = $cpddata->id();
+				$cpds->[$i]->id($cpddata->id());
+			} elsif (defined($hash->{db}->{inchikey}->{$cpds->[$i]->inchikey()})) {
+				my $cpddata = $hash->{db}->{inchikey}->{$cpds->[$i]->inchikey()};
+				#print $cpds->[$i]->id().":missed full match:".$cpddata->{id}.":".$cpddata->{name}.":db\n";
+				$translation->{$cpds->[$i]->id()} = $cpddata->{id}."_c0";
+				$cpds->[$i]->id($cpddata->{id}."_c0");
+			} elsif (defined($hash->{model}->{base}->{$array->[0]})) {
+				my $selected;
+				foreach my $newcpd (keys(%{$hash->{model}->{base}->{$array->[0]}})) {
+					my $cpddata = $hash->{model}->{base}->{$array->[0]}->{$newcpd};
+					if (!defined($selected) || $selected->id() =~ m/_e0$/) {
 						$selected = $cpddata;
 					}
+					#print $cpds->[$i]->id().":missed base match:".$cpddata->id().":".$cpddata->name().":model\n";
 				}
-				print $cpds->[$i]->id().":missed base match:".$cpddata->{id}.":".$cpddata->{name}.":".$cpddata->{reaction_count}.":db\n";
+				#print $cpds->[$i]->id().":selected missed base match:".$selected->id().":".$selected->name().":model\n";
+				$translation->{$cpds->[$i]->id()} = $selected->id();
+				$cpds->[$i]->id($selected->id());
+			} elsif (defined($hash->{db}->{base}->{$array->[0]})) {
+				my $selected;
+				foreach my $newcpd (keys(%{$hash->{db}->{base}->{$array->[0]}})) {
+					my $cpddata = $hash->{db}->{base}->{$array->[0]}->{$newcpd};
+					if (!defined($selected) || $cpddata->{reaction_count} >= $selected->{reaction_count}) {
+						if (!defined($selected)) {
+							$selected = $cpddata;				
+						} elsif ($cpddata->{reaction_count} == $selected->{reaction_count}) {
+							if ($cpddata->{id} < $selected->{id}) {
+								$selected = $cpddata;
+							}
+						} else {
+							$selected = $cpddata;
+						}
+					}
+					#print $cpds->[$i]->id().":missed base match:".$cpddata->{id}.":".$cpddata->{name}.":".$cpddata->{reaction_count}.":db\n";
+				}
+				#print $cpds->[$i]->id().":selected missed base match:".$selected->{id}.":".$selected->{name}.":".$selected->{reaction_count}.":db\n";
+				$translation->{$cpds->[$i]->id()} = $selected->{id}."_c0";
+				$cpds->[$i]->id($selected->{id}."_c0");
 			}
-			print $cpds->[$i]->id().":selected missed base match:".$selected->{id}.":".$selected->{name}.":".$selected->{reaction_count}.":db\n";
-			$translation->{$cpds->[$i]->id()} = $selected->{id}."_c0";
-			$cpds->[$i]->id($selected->{id}."_c0");
+		}
+		if (defined($metabolomics_hash->{$array->[0]})) {
+			foreach my $peakid (keys(%{$metabolomics_hash->{$array->[0]}})) {
+				$peakhits->{all}->{$peakid}->{$cpds->[$i]->id()} = 1;
+				$peakhits->{dam}->{$peakid}->{$cpds->[$i]->id()} = 1;
+				if ($cpds->[$i]->id() =~ m/cpd\d+/) {
+					$cpdhits->{dam}->{$cpds->[$i]->id()}->{$peakid} = 1;
+					$cpdhits->{all}->{$cpds->[$i]->id()}->{$peakid} = 1;
+				} else {
+					$otherhits->{dam}->{$cpds->[$i]->id()}->{$peakid} = 1;
+					$otherhits->{all}->{$cpds->[$i]->id()}->{$peakid} = 1;
+				}
+			}
 		}
 	}
 	if ($cpds->[$i]->id() =~ m/pkc/) {
@@ -175,6 +205,8 @@ for (my $i=0; $i < @{$cpds}; $i++) {
 		$translation->{$cpds->[$i]->id()} = $id;
 		$cpds->[$i]->id($id);
 	}
+	$model_cpd_hash->{$cpds->[$i]->id()} = $cpds->[$i];
+	$totalcpds->{$cpds->[$i]->id()} = 1;
 	if (defined($original_cpd_hash->{$cpds->[$i]->inchikey()}->{damage})) {
 		$cpds->[$i]->dblinks()->{olddmg} = $original_cpd_hash->{$cpds->[$i]->inchikey()}->{damage};
 		$original_cpd_id_hash->{damage}->{$rxns->[$i]->id()} = 1;
@@ -199,6 +231,15 @@ for (my $i=0; $i < @{$rxns}; $i++) {
 		}
 	}
 }
+print "Total damage cpd:".keys(%{$model_cpd_hash})."\n";
+print "Total damage rxn:".@{$rxns}."\n";
+print "Total cpd:".keys(%{$totalcpds})."\n";
+print "Damage peaks:".keys(%{$peakhits->{dam}})."\n";
+print "Damage seed compounds:".keys(%{$cpdhits->{dam}})."\n";
+print "Damage other compounds:".keys(%{$otherhits->{dam}})."\n";
+print "Total peaks:".keys(%{$peakhits->{all}})."\n";
+print "Total seed compounds:".keys(%{$cpdhits->{all}})."\n";
+print "Total other compounds:".keys(%{$otherhits->{all}})."\n";
 
 #Cloaning the damage model to become our new combined model
 my $combined = $dam_model->cloneObject();
@@ -231,22 +272,23 @@ for (my $i=0; $i < @{$rxns}; $i++) {
 $translation = {};
 my $cpds = $rep_model->modelcompounds();
 my $newcpdlist;
+my $repcount = 0;
 for (my $i=0; $i < @{$cpds}; $i++) {	
 	my $array = [split(/[_-]/,$cpds->[$i]->inchikey())];
 	if ($cpds->[$i]->id() =~ m/pkc/ && $cpds->[$i]->id() !~ m/_e0/ && $cpds->[$i]->inchikey() ne "none") {
 		if (defined($hash->{dam}->{inchikey}->{$cpds->[$i]->inchikey()})) {
 			my $cpddata = $hash->{dam}->{inchikey}->{$cpds->[$i]->inchikey()};
-			print $cpds->[$i]->id().":missed full match:".$cpddata->id().":".$cpddata->name().":damage\n";
+			#print $cpds->[$i]->id().":missed full match:".$cpddata->id().":".$cpddata->name().":damage\n";
 			$translation->{$cpds->[$i]->id()} = $cpddata->id();
 			$cpds->[$i]->id($cpddata->id());
 		} elsif (defined($hash->{model}->{inchikey}->{$cpds->[$i]->inchikey()})) {
 			my $cpddata = $hash->{model}->{inchikey}->{$cpds->[$i]->inchikey()};
-			print $cpds->[$i]->id().":missed full match:".$cpddata->id().":".$cpddata->name().":model\n";
+			#print $cpds->[$i]->id().":missed full match:".$cpddata->id().":".$cpddata->name().":model\n";
 			$translation->{$cpds->[$i]->id()} = $cpddata->id();
 			$cpds->[$i]->id($cpddata->id());
 		} elsif (defined($hash->{db}->{inchikey}->{$cpds->[$i]->inchikey()})) {
 			my $cpddata = $hash->{db}->{inchikey}->{$cpds->[$i]->inchikey()};
-			print $cpds->[$i]->id().":missed full match:".$cpddata->{id}.":".$cpddata->{name}.":db\n";
+			#print $cpds->[$i]->id().":missed full match:".$cpddata->{id}.":".$cpddata->{name}.":db\n";
 			$translation->{$cpds->[$i]->id()} = $cpddata->{id}."_c0";
 			$cpds->[$i]->id($cpddata->{id}."_c0");
 		} elsif (defined($hash->{model}->{base}->{$array->[0]})) {
@@ -256,9 +298,9 @@ for (my $i=0; $i < @{$cpds}; $i++) {
 				if (!defined($selected) || $selected->id() =~ m/_e0$/) {
 					$selected = $cpddata;
 				}
-				print $cpds->[$i]->id().":missed base match:".$cpddata->id().":".$cpddata->name().":model\n";
+				#print $cpds->[$i]->id().":missed base match:".$cpddata->id().":".$cpddata->name().":model\n";
 			}
-			print $cpds->[$i]->id().":selected missed base match:".$selected->id().":".$selected->name().":model\n";
+			#print $cpds->[$i]->id().":selected missed base match:".$selected->id().":".$selected->name().":model\n";
 			$translation->{$cpds->[$i]->id()} = $selected->id();
 			$cpds->[$i]->id($selected->id());
 		} elsif (defined($hash->{db}->{base}->{$array->[0]})) {
@@ -276,12 +318,16 @@ for (my $i=0; $i < @{$cpds}; $i++) {
 						$selected = $cpddata;
 					}
 				}
-				print $cpds->[$i]->id().":missed base match:".$cpddata->{id}.":".$cpddata->{name}.":".$cpddata->{reaction_count}.":db\n";
+				#print $cpds->[$i]->id().":missed base match:".$cpddata->{id}.":".$cpddata->{name}.":".$cpddata->{reaction_count}.":db\n";
 			}
-			print $cpds->[$i]->id().":selected missed base match:".$selected->{id}.":".$selected->{name}.":".$selected->{reaction_count}.":db\n";
+			#print $cpds->[$i]->id().":selected missed base match:".$selected->{id}.":".$selected->{name}.":".$selected->{reaction_count}.":db\n";
 			$translation->{$cpds->[$i]->id()} = $selected->{id}."_c0";
 			$cpds->[$i]->id($selected->{id}."_c0");
 		}
+	}
+	if ($cpds->[$i]->id() !~ m/_e0/) {
+		$repcount++;
+		$totalcpds->{$cpds->[$i]->id()} = 1;
 	}
 	my $newcpd = $combined->getObject("modelcompounds",$cpds->[$i]->id());
 	if (!defined($newcpd)) {	
@@ -292,20 +338,21 @@ for (my $i=0; $i < @{$cpds}; $i++) {
 			$cpds->[$i]->id($id);
 		}
 		if ($cpds->[$i]->id() !~ m/_e0/ && ($cpds->[$i]->id() =~ m/cpd\d+/ || defined($metabolomics_hash->{$array->[0]}))) {
-			if ($cpds->[$i]->id() =~ m/cpd\d+/) {
-				$catagories->{cpdid}++;
-				if (defined($metabolomics_hash->{$array->[0]})) {
-					$catagories->{cpd_metabolite_hit}++;
+			if (defined($metabolomics_hash->{$array->[0]})) {
+				foreach my $peakid (keys(%{$metabolomics_hash->{$array->[0]}})) {
+					$peakhits->{all}->{$peakid}->{$cpds->[$i]->id()} = 1;
+					$peakhits->{rep}->{$peakid}->{$cpds->[$i]->id()} = 1;
+					if ($cpds->[$i]->id() =~ m/cpd\d+/) {
+						$otherhits->{rep}->{$cpds->[$i]->id()}->{$peakid} = 1;
+						$otherhits->{all}->{$cpds->[$i]->id()}->{$peakid} = 1;
+					} elsif (defined($metabolomics_hash->{$array->[0]})) {
+						$cpdhits->{rep}->{$cpds->[$i]->id()}->{$peakid} = 1;
+						$cpdhits->{all}->{$cpds->[$i]->id()}->{$peakid} = 1;
+					}
 				}
-			} elsif (defined($metabolomics_hash->{$array->[0]})) {
-				$catagories->{metabolite_hit}++;
 			}
 			$combined->add("modelcompounds",$cpds->[$i]->cloneObject());
-		} elsif ($cpds->[$i]->id() =~ m/_e0/) {
-			$catagories->{extracellular}++;
-		} else {
-			$catagories->{rejected}++;
-		}
+		} 
 	}
 	if (defined($original_cpd_hash->{$cpds->[$i]->inchikey()}->{damage})) {
 		$cpds->[$i]->dblinks()->{olddmg} = $original_cpd_hash->{$cpds->[$i]->inchikey()}->{damage};
@@ -317,12 +364,34 @@ for (my $i=0; $i < @{$cpds}; $i++) {
 	}
 }
 
+print "Total repair compounds:".$repcount."\n";
+print "Repair peaks:".keys(%{$peakhits->{rep}})."\n";
+print "Repair seed compounds:".keys(%{$cpdhits->{rep}})."\n";
+print "Repair other compounds:".keys(%{$otherhits->{rep}})."\n";
+print "Total peaks:".keys(%{$peakhits->{all}})."\n";
+print "Total seed compounds:".keys(%{$cpdhits->{all}})."\n";
+print "Total other compounds:".keys(%{$otherhits->{all}})."\n";
+
+foreach my $cpdid (keys(%{$cpdhits->{db}})) {
+	foreach my $peakid (keys(%{$cpdhits->{db}->{$cpdid}})) {
+		$cpdhits->{all}->{$cpdid}->{$peakid} = 1;
+		$peakhits->{all}->{$peakid}->{$cpdid} = 1;
+		$peakhits->{db}->{$peakid}->{$cpdid} = 1;
+	}
+}
+print "DB peaks:".keys(%{$peakhits->{db}})."\n";
+print "DB seed compounds:".keys(%{$cpdhits->{db}})."\n";
+print "Total peaks:".keys(%{$peakhits->{all}})."\n";
+print "Total seed compounds:".keys(%{$cpdhits->{all}})."\n";
+print "Total other compounds:".keys(%{$otherhits->{all}})."\n";
+
 $cpds = $combined->modelcompounds();
 my $combined_hash = {};
 for (my $i=0; $i < @{$cpds}; $i++) {
 	$combined_hash->{$cpds->[$i]->id()} = 1;
 }
 
+$repcount = 0;
 for (my $i=0; $i < @{$rxns}; $i++) {
 	my $id = $rxns->[$i]->id();	
 	if ($id =~ m/pkr/) {
@@ -346,6 +415,7 @@ for (my $i=0; $i < @{$rxns}; $i++) {
 		}
 	}
 	if ($rxns->[$i]->id() !~ m/transporter_c0/) {
+		$repcount++;
 		my $rgts = $rxns->[$i]->modelReactionReagents();
 		my $add = 1;
 		for (my $j=0; $j < @{$rgts}; $j++) {
@@ -359,6 +429,8 @@ for (my $i=0; $i < @{$rxns}; $i++) {
 		}
 	}
 }
+
+print "Total repair reactions:".$repcount."\n";
 
 #my $wsmeta = $impl->util_save_object($dam_model,"29280/MMSyn3_damage.fix");
 #$wsmeta = $impl->util_save_object($rep_model,"29280/MMSyn3_repair.fix");
@@ -398,13 +470,20 @@ for (my $i=0; $i < @{$rxns}; $i++) {
 #Save the combined model
 my $wsmeta = $impl->util_save_object($combined,"29280/MMSyn3_combined.fix");
 
-print "Extracellular:".$catagories->{extracellular}."\n";
-print "Compound IDs:".$catagories->{cpdid}."\n";
-print "Rejected:".$catagories->{rejected}."\n";
-print "Metabolite hit:".$catagories->{metabolite_hit}."\n";
-print "Compound metabolite hit:".$catagories->{cpd_metabolite_hit}."\n";
-print "Damage metabolite hit:".$catagories->{damage_metabolite_hit}."\n";
-print "Model metabolite hit:".$catagories->{model_metabolite_hit}."\n";
+#Now adding original model to combined model
+$cpds = $model->modelcompounds();
+for (my $i=0; $i < @{$cpds}; $i++) {
+	if (!defined($combined->getObject("modelcompounds",$cpds->[$i]->id()))) {
+		$combined->add("modelcompounds",$cpds->[$i]->cloneObject());
+	}
+}
+$rxns = $model->modelreactions();
+for (my $i=0; $i < @{$rxns}; $i++) {
+	$combined->add("modelreactions",$rxns->[$i]->cloneObject());
+}
+
+#Saving fully combined model
+my $wsmeta = $impl->util_save_object($combined,"29280/FullCombined.fix");
 
 my $match_dmg_cpd = [0,0];
 my $match_rep_cpd = [0,0];
